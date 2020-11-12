42119f3661aef58980a7b15959e54611126c4c1f
carlcgsong <carlcgsong@tencent.com>
2020-11-13 01:08:00 +0800

    feature:commit Hello World

---
+++ Hello World.cpp
@@ -0,0 +1,14 @@
+//
+//  Hellow World.cpp
+//  LeetCode
+//
+//  Created by 宋晨光 on 2020/1/1.
+//  Copyright © 2020 chenguang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";  
+    return 0;
+}
